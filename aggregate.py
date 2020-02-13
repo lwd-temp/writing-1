6f8ea4639a84448b39562e5c9961b942eb38a0f3
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-02-12 21:27:02 -0800

    commit

--- aggregate.py
+++ aggregate.py
@@ -3,25 +3,26 @@
 
 from bs4 import BeautifulSoup
 import cached_url
+import yaml
 
 def getContent(url):
 	content = cached_url.get(url)
-	b = BeautifulSoup(content, 'html.parser')
-	text = b.find('div', {'id': 'container'}).text
-	title = b.find('h1').text
+	content = yaml.load(content, Loader=yaml.FullLoader)
+	b = BeautifulSoup(content['content'], 'html.parser')
+	b.get_text(separator="\n")
 	next_url = None
 	for x in b.find_all('a'):
-		if x.href and x.href.startsWith('https://www.evernote.com/l'):
-			next_url = x.href
+		if x['href'] and x['href'].startswith('https://www.evernote.com/l'):
+			next_url = x['href']
 			break
-	return text, title, next_url
+	return b.get_text(separator="\n"), content['title'], next_url
 
 
 def download(url):
 	filename = None
 	content = []
 	while url:
-		text, title, url = getContent(url)
+		text, title, url = getContent(url + '?json=1')
 		if not filename:
 			filename = title
 		content.append(text)
