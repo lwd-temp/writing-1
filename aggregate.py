afc23f45912e660016cdbbd5356b68239bda2060
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-13 04:25:53 -0700

    commit

--- aggregate.py
+++ aggregate.py
@@ -50,13 +50,12 @@ def downloadDoc(url, filename):
 		f.write(content)
 
 word_count = 0
-download('https://www.evernote.com/l/AO9AYm5PtJtHIZb5W7RvOFPjNGxENZ9uQiI', '面向对象编程')
-download('https://www.evernote.com/l/AO9Nsp2x2-5LBJCMbJvjQNK6zjezsttrIPw', '乐山景然ABO')
-download('https://www.evernote.com/l/AO8Z7ocFEpJJjatcpUFs4oyx1F7g9knqfPA', '学术生涯篇')
-download('https://www.evernote.com/l/AO9X4c31vqVPE5Vs0fHDaQ3INH9qfsne36s', '穿越阵容有点大')
-download('https://www.evernote.com/l/AO8FA3cJQNxEvo5QuwX6vu6GI3n9_KjoRFg', '三界')
-# &includes_info_params=true
-downloadDoc('https://docs.google.com/document/export?format=pdf&id=1gB1hxccoplM1UOJue4DW0HNIgm4Ve1karRmy4zpZ3o8', '妓女的荣耀')
+# download('https://www.evernote.com/l/AO9AYm5PtJtHIZb5W7RvOFPjNGxENZ9uQiI', '面向对象编程')
+# download('https://www.evernote.com/l/AO9Nsp2x2-5LBJCMbJvjQNK6zjezsttrIPw', '乐山景然ABO')
+# download('https://www.evernote.com/l/AO8Z7ocFEpJJjatcpUFs4oyx1F7g9knqfPA', '学术生涯篇')
+# download('https://www.evernote.com/l/AO9X4c31vqVPE5Vs0fHDaQ3INH9qfsne36s', '穿越阵容有点大')
+# download('https://www.evernote.com/l/AO8FA3cJQNxEvo5QuwX6vu6GI3n9_KjoRFg', '三界')
+downloadDoc('https://docs.google.com/document/export?format=pdf&id=1gB1hxccoplM1UOJue4DW0HNIgm4Ve1karRmy4zpZ3o8&includes_info_params=true', '妓女的荣耀')
 with open('word_count.txt', 'a') as f:
 	f.write('%s\t\t%d\n' % (getTime(), word_count))
 command = 'git add . && git commit -m "%s" && git push -u -f'
