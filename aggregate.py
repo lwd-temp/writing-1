56149f5e8b4f7b7f75e286c763a335eebe7fc7db
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-28 09:19:36 -0700

    commit

--- aggregate.py
+++ aggregate.py
@@ -69,6 +69,7 @@ download('https://www.evernote.com/l/AO9Nsp2x2-5LBJCMbJvjQNK6zjezsttrIPw', '乐�
 download('https://www.evernote.com/l/AO8Z7ocFEpJJjatcpUFs4oyx1F7g9knqfPA', '学术生涯篇')
 download('https://www.evernote.com/l/AO9X4c31vqVPE5Vs0fHDaQ3INH9qfsne36s', '穿越阵容有点大')
 download('https://www.evernote.com/l/AO8FA3cJQNxEvo5QuwX6vu6GI3n9_KjoRFg', '三界')
+download('https://www.evernote.com/l/AO_odyI4w7xEj4MmiBa8PLBiDju8GIuJsI0')
 # downloadDoc('https://docs.google.com/document/export?format=zip&id=1gB1hxccoplM1UOJue4DW0HNIgm4Ve1karRmy4zpZ3o8', '妓女的荣耀')
 with open('word_count.txt', 'a') as f:
 	f.write('%s\t\t%d\n' % (getTime(), word_count))
