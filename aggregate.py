df035aa71ce2c6f19ed3f07dffb788de5016135c
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-23 09:06:02 -0700

    commit

--- aggregate.py
+++ aggregate.py
@@ -66,11 +66,11 @@ def download(url, filename = None):
 		result.append('\n\n\n==== %s  ===\n\n\n' % title + text)
 		raw_result.append('\n\n\n==== %s  ===\n\n\n' % title + raw_text)
 	result = clearText(''.join(result))
-	with open('original/%s' % filename, 'w') as f:
+	with open('original/%s.md' % filename, 'w') as f:
 		f.write(result)
-	with open('traditional/%s' % cc.convert(filename), 'w') as f:
+	with open('traditional/%s.md' % cc.convert(filename), 'w') as f:
 		f.write(cc.convert(result))
-	with open('raw/%s' % cc.convert(filename), 'w') as f:
+	with open('raw/%s.md' % cc.convert(filename), 'w') as f:
 		f.write(''.join(raw_result))
 	word_count += countWord(result)
 	print('%s finished.' % filename)
