aebdd9a3ef49afd18b637f86afd2e08c2738c496
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-20 04:08:49 -0700

    commit

--- aggregate.py
+++ aggregate.py
@@ -55,6 +55,10 @@ def getTime():
 def countWord(x):
 	return len([c for c in x if c.isalpha() and ord(c) > 255])
 
+def mkdirs(*args):
+	for arg in args:
+		os.system('mkdir %s > /dev/null 2>&1' % arg)
+
 def download(filename = None, url = None, dirname = 'original'):
 	content = [] # no chapter name, no comments
 	result = [] # with chapter name, no comments
@@ -69,7 +73,10 @@ def download(filename = None, url = None, dirname = 'original'):
 		result.append('\n\n\n==== %s  ===\n\n\n' % title + text)
 		raw_result.append('\n\n\n==== %s  ===\n\n\n' % title + raw_text)
 	result = clearText(''.join(result))
+	mkdirs(dirname, 'txt', 'traditional', 'raw')
 	with open('%s/%s.md' % (dirname, filename), 'w') as f:
+		f.write(result)
+	with open('txt/%s.txt' % filename, 'w') as f:
 		f.write(result)
 	with open('traditional/%s.md' % cc.convert(filename), 'w') as f:
 		f.write(cc.convert(result))
